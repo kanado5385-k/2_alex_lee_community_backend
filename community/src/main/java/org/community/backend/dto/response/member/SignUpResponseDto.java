6aed4107ceba984923789f2063bcc90b301eb438
이윤빈 <kanado20000@naver.com>
2025-03-17 11:47:29 +0900

    feat: 회원가입API 요청에 대한 응답 클래스 추가

---
+++ community/src/main/java/org/community/backend/dto/response/member/SignUpResponseDto.java
@@ -0,0 +1,25 @@
+package org.community.backend.dto.response.member;
+
+import lombok.Getter;
+import org.community.backend.common.response.ApiResponse;
+import org.community.backend.common.response.ResponseCode;
+import org.community.backend.common.response.ResponseMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class SignUpResponseDto extends ApiResponse {
+    private SignUpResponseDto() {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+    }
+
+    public static ResponseEntity<SignUpResponseDto> success(){
+        SignUpResponseDto result = new SignUpResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ApiResponse> duplicateEmail(){
+        ApiResponse result = new ApiResponse(ResponseCode.DUPLICATE_EMAIL, ResponseMessage.DUPLICATE_EMAIL);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+    }
+}
