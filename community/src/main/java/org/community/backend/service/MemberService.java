532b3f0f951eabd9b9beb5d7ac1a3827221d862a
이윤빈 <kanado20000@naver.com>
2025-03-17 11:48:06 +0900

    feat: 회원가입API의 비즈니스 로직 구현

--- community/src/main/java/org/community/backend/service/MemberService.java
+++ community/src/main/java/org/community/backend/service/MemberService.java
@@ -1,6 +1,10 @@
 package org.community.backend.service;
 
+import org.community.backend.common.response.ApiResponse;
+import org.community.backend.dto.response.member.SignUpResponseDto;
 import org.community.backend.repository.JdbcMemberRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 import org.community.backend.member.Member;
@@ -15,17 +19,22 @@ public class MemberService {
     }
 
     @Transactional
-    public void registerUser(String email, String password, String nickname, String profileImage) {
+    public ResponseEntity<? super SignUpResponseDto> registerUser(String email, String password, String nickname, String profileImage) {
+        // <? super SignUpResponseDto> ->  SignUpResponseDto 또는 그 부모 타입(ApiResponse)을 반환할 수 있도록
+
         // 이메일 중복 체크
         if (jdbcMemberRepository.findByEmail(email).isPresent()) {
-            throw new IllegalArgumentException("Email is already taken.");
+            return SignUpResponseDto.duplicateEmail();
         }
 
+        // 사용자 등록
         int memberId = jdbcMemberRepository.save(new Member(email, password, nickname));
 
-        // 프로필 이미지 등록 (이미지가 등록됐을 경우)
+        // 프로필 이미지 등록 (이미지가 존재하는 경우)
         if (profileImage != null && !profileImage.isEmpty()) {
             jdbcMemberRepository.saveProfileImage(memberId, profileImage);
         }
+
+        return SignUpResponseDto.success();
     }
 }
