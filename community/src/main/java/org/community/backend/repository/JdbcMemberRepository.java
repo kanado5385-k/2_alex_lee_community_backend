ba934231d8deb8adad7791cca31f20eda5e894fc
이윤빈 <kanado20000@naver.com>
2025-03-17 10:30:18 +0900

    feat: JdbcMemberRepository 클래스에서 사용자 회원등록하는 메서드 추가

--- community/src/main/java/org/community/backend/repository/JdbcMemberRepository.java
+++ community/src/main/java/org/community/backend/repository/JdbcMemberRepository.java
@@ -4,7 +4,12 @@ import org.community.backend.member.Member;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.Optional;
 
 @Repository
@@ -32,5 +37,25 @@ public class JdbcMemberRepository {
         } catch (EmptyResultDataAccessException e) {
             return Optional.empty();
         }
+    }
+
+    public int save(Member member) {
+        String sql = "INSERT INTO member (email, password, nickname) VALUES (?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder(); // 자동 생성된 기본 키(id)를 저장할 객체
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS); //Statement.RETURN_GENERATED_KEYS -> INSERT 후 생성된 ID 값을 반환받기 위한 옵션
+            ps.setString(1, member.getEmail());
+            ps.setString(2, member.getPassword());
+            ps.setString(3, member.getNickname());
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().intValue();// 생성된 ID 반환
+    }
+
+    public void saveProfileImage(int memberId, String imageUrl) {
+        String sql = "INSERT INTO member_profile_image (member_id, image_url) VALUES (?, ?)";
+        jdbcTemplate.update(sql, memberId, imageUrl);
     }
 }
