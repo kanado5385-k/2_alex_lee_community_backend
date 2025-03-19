df504c33ffcc74eaac1be7410c472645ae7ebcb9
이윤빈 <kanado20000@naver.com>
2025-03-19 15:37:22 +0900

    feat: response code and message 정리

--- community/src/main/java/org/community/backend/common/response/ResponseCode.java
+++ community/src/main/java/org/community/backend/common/response/ResponseCode.java
@@ -1,10 +1,15 @@
 package org.community.backend.common.response;
 
 public class ResponseCode {
+    // 200 or 201
     public static final String SUCCESS = "SU";
-    public static final String BAD_REQUEST = "BR";
-    public static final String INTERNAL_SERVER_ERROR = "ISE";
 
+    // 401
+    public static final String BAD_REQUEST = "BR";
     public static final String DUPLICATE_EMAIL = "DUP";
     public static final String SIGN_IN_FAIL = "SF";
+    public static final String NOT_EXISTED_POST = "NP";
+
+    // 500
+    public static final String INTERNAL_SERVER_ERROR = "ISE";
 }
--- community/src/main/java/org/community/backend/common/response/ResponseMessage.java
+++ community/src/main/java/org/community/backend/common/response/ResponseMessage.java
@@ -1,10 +1,15 @@
 package org.community.backend.common.response;
 
 public class ResponseMessage {
+    // 200 or 201
     public static final String SUCCESS = "Success.";
-    public static final String BAD_REQUEST = "Bad Request.";
-    public static final String INTERNAL_SERVER_ERROR = "Internal Server Error.";
 
+    // 401
+    public static final String BAD_REQUEST = "Bad Request.";
     public static final String DUPLICATE_EMAIL = "Duplicate Email.";
     public static final String SIGN_IN_FAIL = "Login information mismatch.";
+    public static final String NOT_EXISTED_POST = "This post does not exist.";
+
+    // 500
+    public static final String INTERNAL_SERVER_ERROR = "Internal Server Error.";
 }
